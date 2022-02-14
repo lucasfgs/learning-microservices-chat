862050d61f2c676b2e483008490735623a400909
Lucas Ferreira <lucsferreira.dev@gmail.com>
2022-02-14 14:57:10 -0400

    deploy staging

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { run } from './infra/app'
 (async () => {
   const server = await run()
   server.listen(process.env.SERVER_PORT)
-  server.on('listening', () => console.log(`Server listening at port ${process.env.SERVER_PORT}`))
+  server.on('listening', () => console.log(`Server listening at test port ${process.env.SERVER_PORT}`))
 })()
